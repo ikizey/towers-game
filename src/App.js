172ec730c276070df1a7d99200a842242088b413
kizey <ikizey@icloud.com>
2022-11-28 20:21:38 +0400

    add base routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Fragment } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Lobby from './pages/Lobby';
+import CreateGame from './pages/CreateGame';
+import Queue from './pages/Queue';
+import Game from './pages/Game';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          <Route path='/lobby' element={<Lobby />} />
+          <Route path='/create-game' element={<CreateGame />} />
+          <Route path='/queue' element={<Queue />} />
+          <Route path='/game' element={<Game />} />
+        </Route>
+      </Routes>
+    </Fragment>
+  );
 };
 
 export default App;
