0130e6c48c4a84f60bb7e43a01acfa2c0b8d63d2
wizmag <wizblast@yandex.ru>
2022-12-02 21:22:38 +0400

    add show player name

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import papirus from '../Assets/papirus.jpg';
+import usePlayer from '../hooks/usePlayer';
 
 const Home = () => {
+    const nameRef = useRef();
+    const navigate = useNavigate();
+    const { saveName } = usePlayer();
+
+    // console.log(nameRef.current.value);
+    const playHandler = () => {
+        if (nameRef.current.value) {
+            saveName(nameRef.current.value);
+            navigate('/lobby');
+            return;
+        }
+        alert('Enter name, Please!');
+    };
+
     return (
         <div
             style={{
@@ -10,7 +25,7 @@ const Home = () => {
             }}
             className='bg-no-repeat bg-cover w-full flex flex-col h-screen'
         >
-            <div className='bg-gray-200'>
+            <div className='bg-orange-300'>
                 <p className='pb-6 text-center text-6xl text-rose-700 font-bold'>
                     Tower Game
                 </p>
@@ -18,18 +33,19 @@ const Home = () => {
             <div className='text-center pt-12'>
                 <input
                     type='text'
+                    ref={nameRef}
                     placeholder='Enter name'
-                    className='h-14 text-3xl placeholder:font-bold bg-transparent border border-orange-300 rounded-md px-4 text-center font-bold'
+                    className='h-14 text-3xl placeholder:font-bold bg-transparent border-y-4 border-y-orange-300 rounded-md px-4 text-center font-bold'
                 />
             </div>
             <div className='flex justify-center'>
                 <div className='pt-6 flex justify-center  text-8xl font-bold'>
-                    <NavLink
+                    <button
+                        onClick={playHandler}
                         className='font-bold text-10xl p-3 text-yellow-600  hover:text-red-600'
-                        to='/lobby'
                     >
                         Play
-                    </NavLink>
+                    </button>
                 </div>
             </div>
         </div>
