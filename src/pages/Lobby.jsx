b0cbefa9ae850e193549308db2cc59eb5169979f
wizmag <wizblast@yandex.ru>
2022-12-07 12:19:10 +0400

    connect with create game

--- src/pages/Lobby.jsx
+++ src/pages/Lobby.jsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import papirus from '../Assets/papirus.jpg';
 import usePlayer from '../hooks/usePlayer';
+import CreateGame from './CreateGame';
 
 const Lobby = () => {
-    const { playerId, playerName } = usePlayer();
+    const { playerName } = usePlayer();
+    const [createGames, setCreateGames] = useState(false);
+
+    const gameHandler = () => {
+        setCreateGames(true);
+    };
 
     return (
         <div
             style={{
                 backgroundImage: `url(${papirus})`,
             }}
-            className='bg-no-repeat bg-cover w-full flex flex-col h-screen p-8'
+            className='z-10 bg-no-repeat bg-cover w-full flex flex-col h-screen p-8'
         >
             <header>
-                <p className='pb-6 text-center text-5xl text-rose-700 font-bold'>
+                <p className='z-10 pb-6 text-center text-5xl text-rose-700 font-bold'>
                     Tower Game
                 </p>
             </header>
-            <div className='flex flex-1 justify-evenly'>
+            <div
+                className={`z-10 flex flex-1 justify-evenly ${
+                    createGames ? 'blur-sm' : 'blur-none'
+                }`}
+            >
                 <section>
-                    <div className='flex flex-1 gap-6'>
-                        <a
-                            href='#'
+                    <div className='z-10 flex flex-1 gap-6'>
+                        <button
+                            onClick={gameHandler}
                             className='p-4 text-2xl leading-6 rounded-3xl border border-red-800 w-40 block text-center hover:bg-red-400 font-bold hover:scale-110 ease-out duration-200'
                         >
                             Create
-                        </a>
+                        </button>
 
                         <a
                             href='#'
@@ -41,10 +51,10 @@ const Lobby = () => {
                             Queue
                         </a>
                     </div>
-                    <div className='mt-3 text-center p-4 text-2xl leading-6 rounded-3xl border border-red-800'>
+                    <div className='z-10 mt-3 text-center p-4 text-2xl leading-6 rounded-3xl border border-red-800'>
                         <p>Statistic</p>
                     </div>
-                    <div className='flex grow mt-3 text-center p-4 text-2xl leading-6 rounded-3xl border border-red-800'>
+                    <div className='z-10 flex grow mt-3 text-center p-4 text-2xl leading-6 rounded-3xl border border-red-800'>
                         <ul>
                             <li className='border-b-2 border-stone-500'>1</li>
                             <li>2</li>
@@ -54,17 +64,16 @@ const Lobby = () => {
                         </ul>
                     </div>
                 </section>
-                <section className='border border-red-700 block w-1/3 rounded-3xl p-4 text-4xl font-bold'>
+                <section className='z-10 border border-red-700 block w-1/3 rounded-3xl p-4 text-4xl font-bold'>
                     <p className='font-bold text-3xl text-orange-600'>
                         Players
                     </p>
                     <ul>
-                        <li>
-                            {playerName} {playerId}
-                        </li>
+                        <li>{playerName}</li>
                     </ul>
                 </section>
             </div>
+            {createGames && <CreateGame />}
         </div>
     );
 };
