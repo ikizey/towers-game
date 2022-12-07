6e33f202fb8cdcf1fd5e5d140196fe91ae0fa3ef
wizmag <wizblast@yandex.ru>
2022-12-07 17:04:46 +0400

    add NameGameContext

---
+++ src/store/NameGameContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from 'react';
+
+export const NameGameContext = createContext();
+
+const NameGameContextProvider = ({ children }) => {
+    const [nameGame, setNameGame] = useState('');
+    const [playersGame, setPlayersGame] = useState('');
+    const [categoryGame, setCategoryGame] = useState('');
+
+    const saveOptions = (game, players, category) => {
+        console.log(game, players, category);
+        setNameGame(game);
+        setPlayersGame(players);
+        setCategoryGame(category);
+    };
+
+    return (
+        <NameGameContext.Provider
+            value={{ nameGame, playersGame, categoryGame, saveOptions }}
+        >
+            {children}
+        </NameGameContext.Provider>
+    );
+};
+
+export default NameGameContextProvider;
