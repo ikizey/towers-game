74c3e4686689a0e9d16c9a1525d399ef6ef89627
wizmag <wizblast@yandex.ru>
2022-12-07 12:20:37 +0400

    add New Route_Join

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import Game from './pages/Game';
 import Home from './pages/Home';
 import Lobby from './pages/Lobby';
 import Queue from './pages/Queue';
+import Join from './pages/Join';
 
 const App = () => {
-  return (
-    <Fragment>
-      <Routes>
-        <Route path='/'>
-          <Route index element={<Home />} />
-          <Route path='/lobby' element={<Lobby />} />
-          <Route path='/create-game' element={<CreateGame />} />
-          <Route path='/queue' element={<Queue />} />
-          <Route path='/game' element={<Game />} />
-        </Route>
-      </Routes>
-      <ToastContainer />
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Routes>
+                <Route path='/'>
+                    <Route index element={<Home />} />
+                    <Route path='/lobby' element={<Lobby />} />
+                    <Route path='/create-game' element={<CreateGame />} />
+                    <Route path='/queue' element={<Queue />} />
+                    <Route path='/game' element={<Game />} />
+                    <Route path='/join' element={<Join />} />
+                </Route>
+            </Routes>
+            <ToastContainer />
+        </Fragment>
+    );
 };
 
 export default App;
