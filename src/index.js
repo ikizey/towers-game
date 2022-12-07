be57a0bf4afb7556fffeab57af55a67270065e18
wizmag <wizblast@yandex.ru>
2022-12-07 16:47:42 +0400

    add nameGameContextProvider

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import SocketContextProvider from './store/SocketContext';
+import NameGameContextProvider from './store/NameGameContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <SocketContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <NameGameContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </NameGameContextProvider>
     </SocketContextProvider>
   </React.StrictMode>
 );
