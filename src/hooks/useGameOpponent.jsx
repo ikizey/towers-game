f06c4d8c4f295898f1a6cbb5f6a9088d61283bcb
kizey <ikizey@icloud.com>
2022-12-14 21:25:35 +0400

    use useTowers hook

--- src/hooks/useGameOpponent.jsx
+++ src/hooks/useGameOpponent.jsx
@@ -1,43 +1,13 @@
-import { useReducer, useState } from 'react';
-
-const TOWER_ACTIONS = Object.freeze({
-  ADD: 'add',
-  REMOVE: 'remove',
-  DESTROY: 'destroy',
-});
-
-const towersReducer = (state, action) => {
-  const newState = [...state];
-  switch (action.type) {
-    case TOWER_ACTIONS.ADD: {
-      newState[action.slotIndex] = action.cardId;
-      return newState;
-    }
-    case TOWER_ACTIONS.REMOVE: {
-      newState[action.slotIndex] = undefined;
-      return newState;
-    }
-    case TOWER_ACTIONS.DESTROY: {
-      newState[action.slotIndex] = undefined;
-      if (action.slotIndex % 3 === 1) {
-        newState[action.slotIndex - 1] = undefined;
-      }
-      return newState;
-    }
-    default: {
-      return newState;
-    }
-  }
-};
+import { useState } from 'react';
+import useTowers from './useTowers';
 
 const useGameOpponent = (index = -1) => {
   const [opponentId, setOpponentId] = useState(0);
   const [opponentName, setOpponentName] = useState('');
   const [opponentIndex, setOpponentIndex] = useState(index);
   const [opponentHand, setOpponentHand] = useState([]);
-  const [opponentTowers, dispatchTowers] = useReducer(towersReducer, [
-    ...new Array(15),
-  ]);
+  const { towers, towersAddCards, towersRemoveCards, towersDestroy } =
+    useTowers();
 
   const handAddCards = (amount = 1) => {
     setOpponentHand((prev) => [...new Array(prev.length + amount)]);
@@ -45,21 +15,6 @@ const useGameOpponent = (index = -1) => {
 
   const handRemoveCards = (amount = 1) => {
     setOpponentHand((prev) => [...new Array(prev.length - amount)]);
-  };
-
-  const towerAddCards = (slotIndex, cardId) => {
-    const type = TOWER_ACTIONS.ADD;
-    dispatchTowers({ type, slotIndex, cardId });
-  };
-
-  const towerRemoveCards = (slotIndex) => {
-    const type = TOWER_ACTIONS.REMOVE;
-    dispatchTowers({ type, slotIndex });
-  };
-
-  const towerDestroy = (slotIndex) => {
-    const type = TOWER_ACTIONS.DESTROY;
-    dispatchTowers({ type, slotIndex });
   };
 
   return {
@@ -74,10 +29,10 @@ const useGameOpponent = (index = -1) => {
     handAddCards,
     handRemoveCards,
 
-    opponentTowers,
-    towerAddCards,
-    towerRemoveCards,
-    towerDestroy,
+    towers,
+    towersAddCards,
+    towersRemoveCards,
+    towersDestroy,
   };
 };
 
